c060ee30767eae650d39eeec8af808fdade04748
mohamed <engmido1002@gmail.com>
2025-07-09 22:36:03 +0300

    add signout button

--- lib/features/home/presentation/views/home.dart
+++ lib/features/home/presentation/views/home.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:weather_ai/core/utils/app_route.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: IconButton(
+            onPressed: () async {
+              final router = GoRouter.of(context);
+              await FirebaseAuth.instance.signOut();
+
+              router.pushReplacement(AppRoutes.login);
+            },
+            icon: const Icon(Icons.logout)),
+      ),
+    );
   }
 }
